e00298a8b9486fd22769739b8d7631781701a8fb
Gabriela Brito <32690915+gabrielaBrto@users.noreply.github.com>
2023-01-14 11:45:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const connectDB = require('./config/db');
 const app = express();
 
 //CONNECT DATABASE
-//https://github.com/bradtraversy/contact-keeper/blob/master/routes/contacts.js
-//https://cloud.mongodb.com/v2/63a64a04e24b6716cc65db30#/clusters/connect?clusterId=ContactManager
 connectDB();
 
 //INITI MIDDLEWARE
